4c6b6b7a198a901a7752da1a6608e85e0c745b6c
chunchunmaru0000 <cho227675m@gmail.com>
2025-04-27 13:12:26 +0300

    struct ELFH begin

--- src/gner.c
+++ src/gner.c
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+void eeg(const char *msg, struct Inst *i) {
+	fprintf(stderr, "%s:%ld:%ld %s\n", i->file, i->line, i->col, msg);
+	exit(1);
+}
+
 // Byte List Add Times
-void blat(struct BList *l, const uc *s, long t) {
+void blat(struct BList *l, uc *s, long t) {
 	for (long i = 0; i < t; i++)
 		blist_add(l, s[i]);
 }
 
-struct Gner *new_gner(struct PList *is) {
+struct Gner *new_gner(struct PList *is, enum Target t) {
 	struct Gner *g = malloc(sizeof(struct Gner));
+	g->t = t;
 	g->is = is;
 	g->pos = 0;
 	g->prol = new_blist(100);
@@ -18,16 +24,21 @@ struct Gner *new_gner(struct PList *is) {
 	return g;
 }
 
-void gen_Linux_ELF_86_64_prolog(struct Gner *, struct Header *);
-void gen_Linux_ELF_86_64_text(struct Gner *, struct Header *);
+void gen_Linux_ELF_86_64_prolog(struct Gner *, struct ELFH *);
+void gen_Linux_ELF_86_64_text(struct Gner *, long *);
+struct ELFH *new_elfh(struct Gner *, long, long, short, long, short);
 
-void gen(struct Gner *g, enum Target tr) {
-	struct Header *h = new_header();
+void gen(struct Gner *g) {
+	struct ELFH *h;
+	long entry;
 
-	switch (tr) {
+	switch (g->t) {
 	case Linux_ELF_86_64:
-		gen_Linux_ELF_86_64_text(g, h);
+		gen_Linux_ELF_86_64_text(g, &entry);
+		h = new_elfh(g, entry, 0x40, 0x01, 0x00, 0x00);
 		gen_Linux_ELF_86_64_prolog(g, h);
+
+		free(h);
 		break;
 	case Linux_OBJ_86_64:
 		break;
@@ -36,36 +47,49 @@ void gen(struct Gner *g, enum Target tr) {
 	}
 }
 
-void eeg(const char *msg, struct Inst *i) {
-	fprintf(stderr, "%s:%ld:%ld %s\n", i->file, i->line, i->col, msg);
-	exit(1);
+const uc ELFH_MAG[] = {0x7f, 0x45, 0x4c, 0x46};
+const uc ELFH_INDENT[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+const uc ELFH_TYPE_EXEC[] = {0x02, 0x00};
+const uc ELFH_TYPE_DYN[] = {0x03, 0x00}; // not sure about this
+const uc ELFH_MACHINE_AMDx86_64[] = {0x3e, 0x00};
+const uc ELFH_FORMAT_VERSION[] = {0x01, 0x00, 0x00, 0x00}; // only possible
+
+struct ELFH *new_elfh(struct Gner *g, long entrytoff, long phoff, short phn,
+					  long shoff, short shn) {
+	struct ELFH *h = malloc(sizeof(struct ELFH));
+
+	memcpy(h->mag, ELFH_MAG, 4);
+	h->elf_class = 2;	// 2 for 64 bit
+	h->data = 1;		// little endian
+	h->elf_version = 1; // only possible
+	h->osabi = 0;		// UNIX System V ABI
+	memcpy(h->indent, ELFH_INDENT, 8);
+
+	memcpy(h->machine, ELFH_MACHINE_AMDx86_64, 2);
+	memcpy(h->format_version, ELFH_FORMAT_VERSION, 4);
+	memcpy(h->entry, &entrytoff, sizeof(long));
+	memcpy(h->phoff, &phoff, sizeof(long));
+	memcpy(h->shoff, &shoff, sizeof(long));
+
+	switch (g->t) {
+	case Linux_ELF_86_64:
+		memcpy(h->type, ELFH_TYPE_EXEC, 2);
+		break;
+	case Linux_OBJ_86_64:
+		memcpy(h->type, ELFH_TYPE_DYN, 2);
+		break;
+	default:
+		eeg("НЕ ДОСТУПНАЯ ЦЕЛЬ КОМПИЛЯЦИИ НА ДАННЫЙ МОМЕНТ", g->is->st[0]);
+	}
+	return h;
 }
 
-// header MLE - Machine code Linux Elf
-const uc MLE_MAGIC[] = {0x7f, 0x45, 0x4c, 0x46, 0x02, 0x01, 0x01, 0x00,
-						0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-const uc MLE_E_TYPE[] = {0x02, 0x00};
-const uc MLE_E_MACHINE[] = {0x3e, 0x00};
-const uc MLE_E_VERSION[] = {0x01, 0x00, 0x00, 0x00};
 // machine codes
 const uc MLESYSCALL[] = {0x0f, 0x05};
 
-void gen_Linux_ELF_86_64_prolog(struct Gner *g, struct Header *h) {
+void gen_Linux_ELF_86_64_prolog(struct Gner *g, struct ELFH *h) {
 	struct BList *l = g->prol;
-	blat(l, MLE_MAGIC, 9);
-	blat(l, MLE_E_TYPE, 2);
-	blat(l, MLE_E_MACHINE, 2);
-	blat(l, MLE_E_VERSION, 4);
-	blat(l, h->entry, 8);
-	blat(l, h->phoff, 8);
-	blat(l, h->shoff, 8);
-	blat(l, h->flags, 4);
-	blat(l, h->ehsize, 2);
-	blat(l, h->phsize, 2);
-	blat(l, h->phnum, 2);
-	blat(l, h->shsize, 2);
-	blat(l, h->shnum, 2);
-	blat(l, h->shstrndx, 2);
+	blat(l, (uc *)h, sizeof(struct ELFH));
 }
 
 void *alloc_len(long len, long *buf_len) {
@@ -73,10 +97,13 @@ void *alloc_len(long len, long *buf_len) {
 	return malloc(len);
 }
 
-void gen_Linux_ELF_86_64_text(struct Gner *g, struct Header *h) {
+long get_label_offset(char *label) { return 0xb0; }
+
+void gen_Linux_ELF_86_64_text(struct Gner *g, long *entry_off) {
 	long i = 0, buf_len;
 	long *blp = &buf_len; // buf len ptr
 	struct Inst *in;
+	struct Token *tok;
 	uc *ibuff;
 	for (; i < g->is->size; i++) {
 		in = plist_get(g->is, i);
@@ -86,6 +113,12 @@ void gen_Linux_ELF_86_64_text(struct Gner *g, struct Header *h) {
 			ibuff = alloc_len(2, blp);
 			memcpy(ibuff, MLESYSCALL, 2);
 			break;
+		case IENTRY:
+			tok = in->os->st[0];
+			*entry_off = get_label_offset(tok->view);
+			break;
+		default:
+			eeg("НЕИЗВЕСТНАЯ ИНСТРУКЦИЯ", in);
 		}
 		blat(g->text, ibuff, buf_len);
 	}
--- src/gner.h
+++ src/gner.h
@@ -1,6 +1,7 @@
 #include "pser.h"
 
 struct Gner {
+	enum Target t;
 	struct PList *is;
 	long pos;
 	struct BList *prol;
@@ -9,7 +10,16 @@ struct Gner {
 	//	struct BList *bss;
 };
 
-struct Header {
+struct ELFH { // ELF Header
+	uc mag[4];
+	uc elf_class;
+	uc data;
+	uc elf_version;
+	uc osabi;
+	uc indent[8];
+	uc type[2];
+	uc machine[2];
+	uc format_version[4];
 	uc entry[8];
 	uc phoff[8];
 	uc shoff[8];
@@ -22,5 +32,13 @@ struct Header {
 	uc shstrndx[2]; // what this
 };
 
-struct Gner *new_gner(struct PList *);
-void gen(struct Gner *, enum Target);
+struct ELFPH { // ELF Program Header
+
+};
+
+struct ELFSH { // ELF Segment Header
+
+};
+
+struct Gner *new_gner(struct PList *, enum Target t);
+void gen(struct Gner *);
